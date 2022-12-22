1833e16e44e55064492e270d9cdfe93c175793b7
Dreamcatcher Festival <andy@dreamcatcher-festival.com>
2022-12-22 21:30:21 +0100

    start on carousel function

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-let ham = document.getElementById("ham");
+const ham = document.getElementById("ham");
 
 function function1() {
-  ham.classList.toggle("open")
+  ham.classList.toggle("open");
 }
 
 ham.addEventListener("click", function1);
+
+const pos1 = document.getElementById("pos1");
+const pos2 = document.getElementById("pos2");
+const pos3 = document.getElementById("pos3");
