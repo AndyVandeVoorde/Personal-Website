019a43cad5a65e94395f09de2bb03e10f123e106
Dreamcatcher Festival <andy@dreamcatcher-festival.com>
2022-12-23 06:20:01 +0100

    temp commit

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ function function1() {
 
 ham.addEventListener("click", function1);
 
-const pos1 = document.getElementById("pos1");
+// const pos1 = document.getElementById("pos1");
 const pos2 = document.getElementById("pos2");
 const pos3 = document.getElementById("pos3");
+
+function hoverstate() {
+  document.getElementById("pos1").style.backgroundColor = red;
+  console.log("It is working, congratz.")
+}
+
+document.getElementById("pos2").addEventListener("mouseover", console.log("something is happening at least"));
+pos3.addEventListener("mouseover", hoverstate);
