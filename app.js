080cb52b6f2208c409a13ed05165da107a5c2629
Dreamcatcher Festival <andy@dreamcatcher-festival.com>
2022-12-23 07:50:54 +0100

    unfinished second option

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
-const ham = document.getElementById("ham");
+// const ham = document.getElementById("ham");
 
-function function1() {
-  ham.classList.toggle("open");
-}
+// function function1() {
+//   ham.classList.toggle("open");
+// }
 
-ham.addEventListener("click", function1);
+// ham.addEventListener("click", function1);
 
-// const pos1 = document.getElementById("pos1");
+const pos1 = document.getElementById("pos1");
 const pos2 = document.getElementById("pos2");
 const pos3 = document.getElementById("pos3");
+const pos4 = document.getElementById("pos4");
+const pos5 = document.getElementById("pos5");
 
-function hoverstate() {
-  document.getElementById("pos1").style.backgroundColor = red;
-  console.log("It is working, congratz.")
-}
+// function hoverstate() {
+//   pos1.style.boxShadow = "none";
+// }
 
-document.getElementById("pos2").addEventListener("mouseover", console.log("something is happening at least"));
-pos3.addEventListener("mouseover", hoverstate);
+// function undohoverstate() {
+//   pos1.style.boxShadow = "0px 0px 10px 10px rgba(151, 151, 151, 0.5)";
+// }
+
+// pos2.addEventListener("mouseover", hoverstate);
+// pos2.addEventListener("mouseover", changeId2);
+// pos3.addEventListener("mouseover", hoverstate);
+// pos4.addEventListener("mouseover", hoverstate);
+// pos5.addEventListener("mouseover", hoverstate);
+
+// pos2.addEventListener("mouseout", undohoverstate);
+// pos2.addEventListener("mouseout", undoId2);
+// pos3.addEventListener("mouseout", undohoverstate);
+// pos4.addEventListener("mouseout", undohoverstate);
+// pos5.addEventListener("mouseout", undohoverstate);
+
+// function changeId2() {
+//   pos1.id = "pos2"
+//   pos2.id = "hoverlead"
+//   // pos5.id = "invisible"
+// }
+
+// function undoId2() {
+//   pos1.id = "pos1"
+//   pos2.id = "pos2"
+//   // pos5.id = "pos5"
+// }
